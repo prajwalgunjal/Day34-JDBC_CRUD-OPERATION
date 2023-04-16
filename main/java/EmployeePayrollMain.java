c7e27e470cd425175ea4b59a89c2070a772f177d
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-16 23:17:41 +0530

    uc 2 added

--- main/java/EmployeePayrollMain.java
+++ main/java/EmployeePayrollMain.java
@@ -13,13 +13,19 @@ public class EmployeePayrollMain {
             conn = DriverManager.getConnection(url, username, password);
             Statement stmt = null;
             stmt = conn.createStatement();
-            boolean val = stmt.execute("show databases");
+//            boolean val = stmt.execute("show databases");
+            stmt.executeUpdate("CREATE TABLE employee_payroll (id int not null auto_increment,name varchar(30) not null,salary double not null,start date not null,primary key(id))");
+            System.out.println("Created table in given database...");
+            boolean val = stmt.execute("select * from employee_payroll");
             System.out.println(val);
-            if (val){
+           if (val){
                 ResultSet rs = stmt.getResultSet();
                 while (rs.next()) {
                     // Retrieve by column name
-                    System.out.print("Database:- " + rs.getString("Database"));
+                    System.out.print("ID: " + rs.getInt("id"));
+                    System.out.print(", Name: " + rs.getString("name"));
+                    System.out.print(", Salary: " + rs.getDouble("salary"));
+                    System.out.print(", Date: " + rs.getDate("start"));
                     System.out.println();
                 }
             }else {
