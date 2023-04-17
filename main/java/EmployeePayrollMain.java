a9962cee8b86b840f5bfdc9324797631001b5ebd
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-17 11:50:27 +0530

    uc6 added

--- main/java/EmployeePayrollMain.java
+++ main/java/EmployeePayrollMain.java
@@ -17,10 +17,17 @@ public class EmployeePayrollMain {
 //            int count = stmt.executeUpdate("insert into employee_payroll(name,salary,start)values(\"amit\",1000,'2020-01-2'),(\"prajwal\",60000,'2020-08-22'),(\"prathmesh\",600,'2020-08-23')");
 //            System.out.println(count+"Employee inserted successfully");
 //            stmt.executeUpdate("CREATE TABLE employee_payroll (id int not null auto_increment,name varchar(30) not null,salary double not null,start date not null,primary key(id))");
+            int isupdated = stmt.executeUpdate("alter table employee_payroll add gender varchar(20) after name");
+            System.out.println(isupdated);
+            stmt.executeUpdate("update employee_payroll set gender = 'm' where name = 'prajwal'");
+            stmt.executeUpdate("update employee_payroll set gender = 'm' where name = 'amit'");
+            stmt.executeUpdate("update employee_payroll set gender = 'm' where name = 'prathmesh'");
+
 //            System.out.println("Created table in given database...");
 //            boolean val = stmt.execute("select * from employee_payroll");
 //            System.out.println("Showing data present in table");
-            boolean val = stmt.execute("select * from employee_payroll where name = 'prajwal'");
+//            boolean val = stmt.execute("select * from employee_payroll where name = 'prajwal'");
+            boolean val = stmt.execute("select * from employee_payroll");
             System.out.println(val);
            if (val){
                 ResultSet rs = stmt.getResultSet();
@@ -28,6 +35,7 @@ public class EmployeePayrollMain {
                     // Retrieve by column name
                     System.out.print("ID: " + rs.getInt("id"));
                     System.out.print(", Name: " + rs.   getString("name"));
+                    System.out.print(", gender: " + rs.   getString("gender"));
                     System.out.print(", Salary: " + rs.getDouble("salary"));
                     System.out.print(", Date: " + rs.getDate("start"));
                     System.out.println();
